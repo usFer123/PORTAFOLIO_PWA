8fa0153c1abe569e68dd62e06095fe8eeba0e260
Fernanda Perez <fernandaperez76437@gmail.com>
2024-12-08 00:22:36 -0600

    cambios al service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,78 +1,22 @@
-const CACHE_NAME = "FP_cache_PWA_MFRP_v1"; // Versión agregada para controlar actualizaciones
-
+const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-    './',
-    './assets/img/c.png',
-    './assets/img/Captura de pantalla 2024-12-07 152923.png',
-    './assets/img/Captura de pantalla 2024-12-07 153448.png',
-    './assets/img/Captura de pantalla 2024-12-07 160832.png',
-    './assets/img/fondo.png',
-    './assets/img/foto.png',
-    './assets/img/java.png',
-    './assets/img/logo.png',
-    './assets/img/node.png',
-    './assets/img/python.png',
-    './assets/img/react.png',
-    './assets/img/react2.png',
-    './assets/img/TASKEASE.png',
-    './index.html',
-    './js/script.js',
-    './css/styles.css',
-    './manifest.json'
-
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/css/styles.css',
+    '/js/script.js'
 ];
 
-// Evento de instalación - caché de archivos
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(urlsToCache)
-                .then(() => self.skipWaiting()); // Invocar correctamente el método
-        })
+        .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-
-// Evento de activación - limpieza de caché antiguo
-self.addEventListener('activate', e => {
-    const listaBlancaCache = [CACHE_NAME];
-
-    e.waitUntil(
-        caches.keys()
-        .then(nombresCache => {
-            return Promise.all(
-                nombresCache.map(nombresCache => {
-                    if (listaBlancaCache.indexOf(nombresCache) === -1) {
-                        return caches.delete(nombresCache);
-                    }
-                })
-            );
-        })
-        // Activar el nuevo caché
-        .then(() => self.clients.claim())
-    );
-});
-
-// Evento de obtención de archivos - responder desde la caché o buscar en la red
 self.addEventListener('fetch', e => {
     e.respondWith(
         caches.match(e.request)
-        .then(res => {
-            if (res) {
-                return res; // Responder desde la caché si existe
-            }
-            return fetch(e.request); // Buscar en la red si no está en caché
-        })
+        .then(response => response || fetch(e.request))
     );
 });
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(function(registration) {
-            console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(function(error) {
-            console.log('Error al registrar el Service Worker:', error);
-        });
-}

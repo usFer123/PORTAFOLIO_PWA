20954082f2c120ca49ea321591800e743d81fb0b
Fernanda Perez <fernandaperez76437@gmail.com>
2024-12-07 23:42:42 -0600

    cambios al service worker y archivo manifiesto, otra vez

--- serviceworker.js
+++ serviceworker.js
@@ -60,3 +60,13 @@ self.addEventListener('fetch', e => {
         })
     );
 });
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+            console.log('Service Worker registrado con éxito:', registration);
+        })
+        .catch(function(error) {
+            console.log('Error al registrar el Service Worker:', error);
+        });
+}
